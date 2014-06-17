3f2631c3f4e44f878d8142d830db5b4f2035f2ae
pigram86 <todd@toddpigram.com>
2014-06-17 11:29:00 -0400

    Initial Commit

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,7 @@ maintainer_email 'todd@toddpigram.com'
 license          'Apache 2.0'
 description      'Installs/Configures win8_2012_vdibasline'
 long_description 'Installs/Configures win8_2012_vdibasline'
-version          '0.1.0'
+version          '1.0.0'
+depends          "windows"
 
+supports 'windows'
